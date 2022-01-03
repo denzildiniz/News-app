e283e01716124597a4ae570b7230bee40ad68b4f
denzildiniz <denzildiniz73@gmail.com>
2022-01-03 17:12:16 +0530

    made statics app using static data

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import NewsItem from '../newsItem/NewsItem'
 
+import article from '../../smaple.json'
+import { useState } from 'react'
+
+import { Grid } from '@material-ui/core'
+
 const News = () => {
+    const [data, setData] = useState(article)
+    console.log(data.articles)
     return (
         <div>
-            newscomponet
-            <NewsItem />
+            <Grid container spacing={2}>
+                {data.articles?.map((news) => (
+                    <Grid item xs={12} sm={4} md={3} key={news.url}>
+                        <NewsItem
+                            title={news.title}
+                            description={news.description}
+                            urlToImage={news.urlToImage}
+                            url={news.url}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
 
 export default News
+
+
+
--- src/components/newsItem/NewsItem.jsx
+++ src/components/newsItem/NewsItem.jsx
@@ -1,10 +1,30 @@
+import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core'
 import React from 'react'
 
-const NewsItem = () => {
+
+const NewsItem = ({ title, description, urlToImage, url }) => {
     return (
-        <div>
-            this is a news item
-        </div>
+        <Card>
+            <CardMedia
+                component="img"
+                alt={urlToImage}
+                height='auto'
+                image={urlToImage}
+            />
+            <CardContent>
+                <Typography component='h4' gutterBottom >
+                    {title}
+                </Typography>
+                <Typography variant='body2' color="textSecondary" component="p">
+                    {description}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size='small' color='primary' href={url} target='_blank' >
+                    Read More...
+                </Button>
+            </CardActions>
+        </Card>
     )
 }
 
