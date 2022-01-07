75703f1860c3900ac6cbaacafd55a3e66acfed1f
denzildiniz <denzildiniz73@gmail.com>
2022-01-07 19:05:58 +0530

    fully functioniny with infinite loading app developed

--- src/components/news/News.jsx
+++ src/components/news/News.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 import { Grid } from '@material-ui/core'
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -19,36 +19,31 @@ const News = ({ pageSize, category }) => {
     // const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        getNews().then(res => {
-            // console.log({ res })
-            // console.log(res.data.totalResults)
-            setData(res.data);
-            // console.log(data)
-            setTotalResults(res.data.totalResults);
-            setPage(page+1);
-            // console.log(test)
-        })
-
-    }, [totalResults])
+        getNews();
+    }, [])
 
     const getNews = async () => {
-        const recievedNews = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=44050a4f3f6a48308b27288739475db6&page=${page}&pageSize=${pageSize}`);
-        // console.log({recievedNews})
-        // console.log({totalResults})
-        return recievedNews;
+        const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=44050a4f3f6a48308b27288739475db6&page=${page}&pageSize=${pageSize}`;
+            const fetchedData = await fetch(url);
+            const parsedData = await fetchedData.json();
+            console.log(parsedData.articles)
+            setData(parsedData.articles);
+            setTotalResults(parsedData.totalResults);
+            setPage(page + 1);
     }
 
     const fetchMoreData = async () => {
-        setPage(page + 1);
-        const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=44050a4f3f6a48308b27288739475db6&page=${page}&pageSize=${pageSize}`;
-        const fetchedData = await fetch(url);
-        const parsedData = await fetchedData.json();
-        // console.log(data)
-        console.log({parsedData});
-        setData(data.concat(parsedData.articles));
-        // setData([...data, parsedData.articles]);
-        setTotalResults(parsedData.totalResults);
-        // console.log({ data })
+        try {
+            setPage(page + 1);
+            const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=44050a4f3f6a48308b27288739475db6&page=${page}&pageSize=${pageSize}`;
+            const fetchedData = await fetch(url);
+            const parsedData = await fetchedData.json();
+            setData(data.concat(parsedData.articles));
+            setTotalResults(parsedData.totalResults);
+        } catch (error) {
+            console.log({ error })
+        }
+
     }
 
     const classes = useStyles();
@@ -63,7 +58,7 @@ const News = ({ pageSize, category }) => {
                 style={{ 'overflow': 'hidden' }}
             >
                 <Grid container spacing={2} >
-                    {data.articles && data.articles.map((article) => (
+                    {data && data.map((article) => (
                         <Grid item xs={12} sm={6} md={4} key={article.url}>
                             <NewsItem
                                 title={article.title}
